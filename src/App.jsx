58a9968595f55934d3ffbd20e8a0d656ae2ff3d8
tunetune2424 <s21651266@gmail.com>
2026-04-22 18:49:39 +0900

    init: mikan farm website project

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
+import Nav from './components/Nav'
+import Hero from './components/Hero'
+import About from './components/About'
+import Products from './components/Products'
+import Philosophy from './components/Philosophy'
+import Contact from './components/Contact'
 import Footer from './components/Footer'
-import Home from './pages/Home'
-import About from './pages/About'
-import Products from './pages/Products'
-import Commitment from './pages/Commitment'
-import Contact from './pages/Contact'
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/commitment" element={<Commitment />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+    <>
+      <Nav />
+      <main>
+        <Hero />
+        <About />
+        <Products />
+        <Philosophy />
+        <Contact />
       </main>
       <Footer />
-    </div>
+    </>
   )
 }
 
